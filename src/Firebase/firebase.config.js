f8c3074c24cef0a71c3ee97cf9c27b031a3f2ee9
rajibdas-it <rajibrad@gmail.com>
2022-10-18 11:40:44 +0600

    project init

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBDkxSzXbbj-DxkTW2H8dAsauIXExgIzuk",
+  authDomain: "pocket-news-249af.firebaseapp.com",
+  projectId: "pocket-news-249af",
+  storageBucket: "pocket-news-249af.appspot.com",
+  messagingSenderId: "512998601852",
+  appId: "1:512998601852:web:a68c17e8959ad955df85f9",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
